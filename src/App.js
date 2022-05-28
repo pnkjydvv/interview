fa22897421a0eae195ea99a962151a11cb010c31
Pankaj Yadav <pk469349@gmail.com>
2022-05-28 13:52:35 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 import Discription from './Component/Discription';
 import Header from './Component/Header';
 import './Discription.css';
